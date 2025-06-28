1afc0443cf511f6e07164d09c410809060d989a5
defendimattia <defendi.mattia92@gmail.com>
2025-06-28 11:35:00 +0200

    Add Passerotto override methods

--- src/org/lessons/abstractANDinterface/Passerotto.java
+++ src/org/lessons/abstractANDinterface/Passerotto.java
@@ -1,5 +1,15 @@
 package org.lessons.abstractANDinterface;
 
-public class Passerotto extends Animale{
+public class Passerotto extends Animale {
+
+    @Override
+    protected void verso() {
+        System.out.println("Cip cip!");
+    }
+
+    @Override
+    protected void mangia() {
+        System.out.println("Il passerotto mangia semi e insetti");
+    };
 
 }
