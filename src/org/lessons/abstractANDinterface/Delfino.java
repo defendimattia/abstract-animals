6a3a61f6574242de8ae939231f159b75a8f6c1ec
defendimattia <defendi.mattia92@gmail.com>
2025-06-28 11:34:14 +0200

    Add Delfino override methods

--- src/org/lessons/abstractANDinterface/Delfino.java
+++ src/org/lessons/abstractANDinterface/Delfino.java
@@ -1,5 +1,14 @@
 package org.lessons.abstractANDinterface;
 
 public class Delfino extends Animale{
+    
+    @Override
+    protected void verso() {
+        System.out.println("Click-click!");
+    }
+
+    @Override
+    protected void mangia() {
+        System.out.println("Il delfino mangia pesce");    };
 
 }
