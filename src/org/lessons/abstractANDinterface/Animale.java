1a2ee6b337ba6fc6219478f949352086a6df9244
defendimattia <defendi.mattia92@gmail.com>
2025-06-28 11:28:22 +0200

    Add abstract methods on Animale class

--- src/org/lessons/abstractANDinterface/Animale.java
+++ src/org/lessons/abstractANDinterface/Animale.java
@@ -1,5 +1,12 @@
 package org.lessons.abstractANDinterface;
 
 public abstract class Animale {
-    
+
+    protected void dormi() {
+        System.out.println("Zzz");
+    }
+
+    protected abstract void verso();
+    protected abstract void mangia();
+
 }
