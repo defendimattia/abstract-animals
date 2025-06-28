d2a03cef12073e958ea04f5cef3f5fc714e18aaf
defendimattia <defendi.mattia92@gmail.com>
2025-06-28 11:33:13 +0200

    Add Cane override methods

--- src/org/lessons/abstractANDinterface/Cane.java
+++ src/org/lessons/abstractANDinterface/Cane.java
@@ -1,5 +1,14 @@
 package org.lessons.abstractANDinterface;
 
-public class Cane extends Animale{
+public class Cane extends Animale {
 
+    @Override
+    protected void verso() {
+        System.out.println("Bau!");
+    }
+
+    @Override
+    protected void mangia() {
+        System.out.println("Il cane mangia le crocchette");
+    };
 }
