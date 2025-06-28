f5d3929f85a55b1e338a20337aa5d98a5b0a8e1f
defendimattia <defendi.mattia92@gmail.com>
2025-06-28 11:32:15 +0200

    Add Aquila override methods

--- src/org/lessons/abstractANDinterface/Aquila.java
+++ src/org/lessons/abstractANDinterface/Aquila.java
@@ -1,5 +1,14 @@
 package org.lessons.abstractANDinterface;
 
-public class Aquila extends Animale{
+public class Aquila extends Animale {
 
+    @Override
+    protected void verso() {
+        System.out.println("Screech!");
+    }
+
+    @Override
+    protected void mangia() {
+        System.out.println("L'aquila mangia piccoli animali");
+    };
 }
